f1c440f715f678021f1665863f8153909f258e53
chenGuoFeng <chenguofeng@xk.design>
2025-04-25 23:10:16 +0800

    fix: 修复nuxt

--- vue2-template/src/router/index.js
+++ vue2-template/src/router/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: chenGuoFeng chenguofeng@xk.design
+ * @Date: 2025-04-06 12:38:34
+ * @LastEditors: chenGuoFeng chenguofeng@xk.design
+ * @LastEditTime: 2025-04-12 23:31:53
+ * @FilePath: /cool-cli-template/vue2-template/src/router/index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
@@ -20,7 +28,7 @@ const routes = [
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
+  base: '/',
   routes
 })
 
